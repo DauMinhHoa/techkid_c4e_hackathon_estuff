a2ef6a7b5910804c03ff49d5bebdf266bf0af3aa
Hoanh An <hoanhan101@gmail.com>
2016-06-09 15:44:45 +0700

    change name

--- test mlab.py
+++ test mlab.py
@@ -5,56 +5,24 @@ db = pymongo.MongoClient(db_uri).get_default_database()
 
 post_collection = db["post"]
 
+# while True:
+print("<Have>")
+title = input("Title:")
+catagoryHave = input("Catagory:")
+overallHave = int(input("Overall:"))
+priceHave = int(input("Price:"))
 
-# print("<Have>")
-# title = input("Title:")
-# overall = int(input("Overall:"))
-# catagory = input("Catagory:")
-# price = int(input("Price:"))
-#
-# print("<Need>")
-# catagoryNeed = input("Catagory:")
-# start_overallNeed = int(input("Overall from:"))
-# end_overallNeed = int(input("Overall to:"))
-# start_priceNeed = int(input("Price from:"))
-# end_priceNeed = int(input("Price to:"))
-#
-#
-# post_collection.insert_one({"title":title, "overall": overall, "catagory": catagory, "price":price,
-#                             "catagoryNeed": catagoryNeed, "start_overallNeed":start_overallNeed, "end_overallNeed":end_overallNeed, "start_priceNeed":start_priceNeed, "end_priceNeed":end_priceNeed})
+print("<Need>")
+catagoryNeed = input("Catagory:")
+min_overallNeed = int(input("Overall from:"))
+max_overallNeed = int(input("Overall to:"))
+min_priceNeed = int(input("Price from:"))
+max_priceNeed = int(input("Price to:"))
+
+
+post_collection.insert_one({"title":title, "catagoryHave": catagoryHave, "overallHave": overallHave, "priceHave":priceHave,
+                            "catagoryNeed": catagoryNeed, "min_overallNeed":min_overallNeed, "max_overallNeed":max_overallNeed, "min_priceNeed":min_priceNeed, "max_priceNeed":max_priceNeed})
 
 all_post = post_collection.find()
 for x in all_post:
-    print(x["title"])
-
-
-# class Post():
-#     def __init__(self, name, title, overall, catagory, price, catagoryNeed, start_overallNeed, end_overallNeed, start_priceNeed, end_priceNeed):
-#         self.name = name
-#         self.title = title
-#         self.overall = int(overall)
-#         self.catagory = catagory
-#         self.price = int(price)
-#
-#         self.catagoryNeed = catagoryNeed
-#         self.start_overallNeed = int(start_overallNeed)
-#         self.end_overallNeed = int(end_overallNeed)
-#         self.start_priceNeed = int(start_priceNeed)
-#         self.end_priceNeed = int(end_priceNeed)
-#
-#     def showPost(self):
-#         print(self.name, "- has", self.title, self.overall, self.catagory, self.price,
-#               "\n\t - need",
-#               self.catagoryNeed, self.start_overallNeed, self.end_overallNeed, self.start_priceNeed, self.end_priceNeed)
-#
-#     def matchNeed(self, other):
-#         if self.catagoryNeed == other.catagory:
-#             if self.start_overallNeed < other.overall < self.end_overallNeed + 1:
-#                 if self.start_priceNeed < other.price < self.end_priceNeed:
-#                     print("Match")
-#                 else:
-#                     print("Unmatch price")
-#             else:
-#                 print("Unmatch overall")
-#         else:
-#             print("Unmatch catagory")
+    print(x)
--- test.py
+++ test.py
@@ -1,49 +1,73 @@
 class Post():
-    def __init__(self, name, title, overall, catagory, price, catagoryNeed, start_overallNeed, end_overallNeed, start_priceNeed, end_priceNeed):
+    def __init__(self, name, title, catagoryHave, overallHave, priceHave, catagoryNeed, min_overallNeed, max_overallNeed, min_priceNeed, max_priceNeed):
+        # Have
         self.name = name
         self.title = title
-        self.overall = int(overall)
-        self.catagory = catagory
-        self.price = int(price)
-
+        self.catagoryHave = catagoryHave
+        self.overallHave = int(overallHave)
+        self.priceHave = int(priceHave)
+        # Need
         self.catagoryNeed = catagoryNeed
-        self.start_overallNeed = int(start_overallNeed)
-        self.end_overallNeed = int(end_overallNeed)
-        self.start_priceNeed = int(start_priceNeed)
-        self.end_priceNeed = int(end_priceNeed)
+        self.min_overallNeed = int(min_overallNeed)
+        self.max_overallNeed = int(max_overallNeed)
+        self.min_priceNeed = int(min_priceNeed)
+        self.max_priceNeed = int(max_priceNeed)
 
     def showPost(self):
-        print(self.name, "- has", self.title, self.overall, self.catagory, self.price,
+        print(self.name, "- has", self.title, self.catagoryHave, self.overallHave, self.priceHave,
               "\n\t - need",
-              self.catagoryNeed, self.start_overallNeed, self.end_overallNeed, self.start_priceNeed, self.end_priceNeed)
+              self.catagoryNeed, self.min_overallNeed, self.max_overallNeed, self.min_priceNeed, self.max_priceNeed)
 
-    def matchNeed(self, other):
-        if self.catagoryNeed == other.catagory:
-            if self.start_overallNeed < other.overall < self.end_overallNeed + 1:
-                if self.start_priceNeed < other.price < self.end_priceNeed:
-                    print("Match")
+    def meetNeed(self, other):
+        if self.catagoryHave == other.catagoryNeed and self.catagoryNeed == other.catagoryHave:
+            if other.min_overallNeed <= self.overallHave <= other.max_overallNeed and self.min_overallNeed <= other.overallHave <= self.max_overallNeed:
+                if other.min_priceNeed <= self.priceHave <= other.max_priceNeed and self.min_priceNeed <= other.priceHave <= self.max_priceNeed:
+                    print("Meet")
                 else:
-                    print("Unmatch price")
+                    print("not meet price")
             else:
-                print("Unmatch overall")
+                print("not meet overall")
         else:
-            print("Unmatch catagory")
+            print("not meet catagory")
 
-huy = Post("Huy","Iphone 5 black 64GB fullbox", 99, "iOS", 5000000, "Android", 90, 100, 4000000, 7000000)
+huy = Post("Huy","Iphone 5 black 64GB fullbox", "iOS", 100, 6000000, "Android", 95, 100, 4000000, 7000000)
 huy.showPost()
 
-hiep = Post("Hiep","Note 4 white 32GB likenew", 95, "Android", 6000000, "iOs", 90, 100, 4000000, 7000000)
+hiep = Post("Hiep","Note 4 white 32GB likenew", "Android", 95, 6000000, "Android", 90, 100, 4000000, 7000000)
 hiep.showPost()
 
-hoang = Post("Hoang", "Xperia Z 32GB used", 70, "Android", 3000000, "Android", 70, 90, 2000000, 5000000)
+hoang = Post("Hoang", "Xperia Z 32GB used", "Android", 95, 6000000, "Android", 70, 100, 2000000, 5000000)
 hoang.showPost()
 
-khanh = Post("Khanh", "Moto X 64GB brandnew", 100, "Android", 10000000, "iOS", 99, 100, 4000000, 7000000)
+khanh = Post("Khanh", "Moto X 64GB brandnew", "Android", 100, 10000000, "iOS", 99, 100, 4000000, 7000000)
 khanh.showPost()
 
-huy.matchNeed(hiep)
-huy.matchNeed(hoang)
-huy.matchNeed(khanh)
-hiep.matchNeed(hoang)
+nguyen = Post("Nguyen", "OnepPLus One 64GB fullbox", "Android", 96, 6000000, "iOS", 90, 100, 4000000, 7000000)
+nguyen.showPost()
+
+huy.meetNeed(hoang)
+huy.meetNeed(khanh)
+hiep.meetNeed(hoang)
+huy.meetNeed(nguyen)
 
 
+# data = [hiep, huy, hoang, khanh, nguyen]
+# for i in data:
+#     print(i)
+
+# print("<Have>")
+# name = input("Name:")
+# title = input("Title:")
+# catagoryHave = input("Catagory:")
+# overallHave = int(input("Overall:"))
+# priceHave = int(input("Price:"))
+#
+# print("<Need>")
+# catagoryNeed = input("Catagory:")
+# min_overallNeed = int(input("Min Overall:"))
+# max_overallNeed = int(input("Max Overall:"))
+# min_priceNeed = int(input("Min Price:"))
+# max_priceNeed = int(input("Max Price:"))
+#
+# hoanh = Post(name, title, catagoryHave, overallHave, priceHave, catagoryNeed, min_overallNeed, max_overallNeed, min_priceNeed, max_priceNeed)
+# hoanh.showPost()
